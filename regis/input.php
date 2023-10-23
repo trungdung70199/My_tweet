3c80e115f5d0a41cb586ddb170845c2429504ad0
trungdung70199 <trungdung70199@gmail.com>
2023-10-23 14:29:58 +0900

    up input

--- regis/input.php
+++ regis/input.php
@@ -1,14 +1,17 @@
 <?php
-// Session
-session_start();
-session_regenerate_id(true);
+require_once('../app.php');
 
-//var_dump($_SESSION['regis']);
-if (!empty($_SESSION['regis'])) {
-    $regis = $_SESSION['regis'];
+if (!empty($_SESSION['regist'])) {
+    $regist = $_SESSION['regist'];
+}
+
+//セッションにエラーがあれば取得
+if (!empty($_SESSION['errors'])) {
+    //セッションのエラー削除
+    $errors = $_SESSION['errors'];
+    unset($_SESSION['errors']);
 }
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -21,22 +24,26 @@ if (!empty($_SESSION['regis'])) {
 <body>
     <main id="id" class="d-flex justify-content-center">
         <div class="w-50 mt-3 p-5 bg-light">
-            <h2 class="h2 mb-3 fw-normal">Regis</h2>
-            <form action="" method="post">
+            <h2 class="h2 mb-3 fw-normal text-center">Regist</h2>
+            <form action="confirm.php" method="post">
                 <div class="form-floating mb-2">
-                    <input type="text" name="name" class="form-control">
+                    <input type="text" name="name" value="<?= @$regist['name'] ?>" class="form-control">
                     <label for="" class="form-label">Name</label>
+                    <p class="text-danger"><?= @$errors['name'] ?></p>
                 </div>
                 <div class="form-floating mb-2">
-                    <input type="email" name="email" class="form-control">
+                    <input type="email" name="email" value="<?= @$regist['email'] ?>" class="form-control">
                     <label for="" class="form-label">Email</label>
+                    <p class="text-danger"><?= @$errors['email'] ?></p>
                 </div>
                 <div class="form-floating mb-2">
                     <input type="password" name="password" class="form-control">
                     <label for="" class="form-label">Password</label>
+                    <p class="text-danger"><?= @$errors['password'] ?></p>
                 </div>
                 <div>
-                    <button class="w-100 btn btn-primary">Regis</button>
+                    <button class="w-100 mb-2 btn btn-primary">Regist</button>
+                    <a href="../login/" class="w-100 btn btn-outline-primary">Sign in</a>
                 </div>
             </form>
         </div>
